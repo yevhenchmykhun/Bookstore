25bf14cfb0a246d88405fac08e5b872870c244f5
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2019-10-16 20:11:26 +0200

    Connect REST API and repository layer

--- Bookstore/src/main/java/com/yevhenchmykhun/restapi/BookRestController.java
+++ Bookstore/src/main/java/com/yevhenchmykhun/restapi/BookRestController.java
@@ -1,40 +1,43 @@
 package com.yevhenchmykhun.restapi;
 
 import com.yevhenchmykhun.entity.Book;
+import com.yevhenchmykhun.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookRestController {
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping
     public List<Book> getAll() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book());
-        books.add(new Book());
-        return books;
+        return bookRepository.findAll();
     }
 
     @GetMapping(path = "/{id}")
     public Book get(@PathVariable Long id) {
-        return new Book();
+        return bookRepository.getOne(id);
     }
 
     @PostMapping
     public Book post(@RequestBody Book book) {
-        return book;
+        return bookRepository.saveAndFlush(book);
     }
 
     @PutMapping(path = "/{id}")
     public Book put(@PathVariable Long id, @RequestBody Book book) {
-        return book;
+        book.setId(id);
+        return bookRepository.saveAndFlush(book);
     }
 
     @DeleteMapping(path = "/{id}")
     public void delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
     }
 
 }
--- Bookstore/src/test/com/yevhenchmykhun/restapi/BookRestControllerIntegrationTest.java
+++ Bookstore/src/test/com/yevhenchmykhun/restapi/BookRestControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.yevhenchmykhun.restapi;
 
 import com.yevhenchmykhun.entity.Book;
+import com.yevhenchmykhun.repository.BookRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,8 +28,8 @@ public class BookRestControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
-//    @MockBean
-//    private BookService service;
+    @MockBean
+    private BookRepository repository;
 
     @Test
     public void givenBooks_whenGetAllBooks_thenReturnJsonArray() throws Exception {
@@ -37,13 +39,13 @@ public class BookRestControllerIntegrationTest {
 
         List<Book> books = Collections.singletonList(book);
 
-//        given(service.getAllBooks()).willReturn(books);
+        given(repository.findAll()).willReturn(books);
 
         mvc.perform(get("/books")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)));
-//                .andExpect(jsonPath("$[0].name", is(books.get(0).getName())));
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(books.get(0).getName())));
     }
 
 }
