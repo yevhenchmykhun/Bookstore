d6a069b46e1661cdeb05b04aa3ceba1998afa2b1
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2019-10-18 17:04:42 +0200

    Config Thymeleaf

---
+++ Bookstore/src/main/java/com/yevhenchmykhun/config/ThymeleafConfig.java
@@ -0,0 +1,63 @@
+package com.yevhenchmykhun.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+public class ThymeleafConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/view/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setTemplateEngineMessageSource(messageSource());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**", "/css/**", "/js/**", "/img/**")
+                .addResourceLocations("/resources/", "/resources/css/", "/resources/js/", "/resources/img/");
+    }
+
+}
--- Bookstore/src/main/java/com/yevhenchmykhun/controller/IndexController.java
+++ Bookstore/src/main/java/com/yevhenchmykhun/controller/IndexController.java
@@ -1,46 +1,31 @@
 package com.yevhenchmykhun.controller;
 
-import com.yevhenchmykhun.repository.BookRepository;
-import com.yevhenchmykhun.repository.RepositoryFactory;
 import com.yevhenchmykhun.entity.Book;
+import com.yevhenchmykhun.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/index")
-public class IndexController extends HttpServlet {
+@Controller
+public class IndexController {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
+    @Autowired
+    private BookRepository bookRepository;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        getServletContext().setAttribute("categories", new RepositoryFactory().getCategoryRepository().findAll());
-
-        BookRepository bookRepository = new RepositoryFactory().getBookRepository();
-//        List<Book> pageOne = bookDao.getPage(2, 1, 6);
-//        List<Book> pageTwo = bookDao.getPage(2, 2, 6);
+    @GetMapping("/index")
+    public String get(Model model) {
 
         List<Book> pageOne = bookRepository.findAllByCategoryId(2L, PageRequest.of(0, 6));
         List<Book> pageTwo = bookRepository.findAllByCategoryId(2L, PageRequest.of(1, 6));
 
-        request.setAttribute("pageOne", pageOne);
-        request.setAttribute("pageTwo", pageTwo);
+        model.addAttribute("pageOne", pageOne);
+        model.addAttribute("pageTwo", pageTwo);
 
-        String url = "/WEB-INF/view/index.jsp";
-        request.getRequestDispatcher(url).forward(request, response);
-
+        return "index";
     }
 
 }
