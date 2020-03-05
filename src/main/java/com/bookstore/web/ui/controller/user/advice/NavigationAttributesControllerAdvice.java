4128d68d3bb79efdfa080d2331f4dc7160d8761b
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2020-03-05 19:25:13 +0100

    Apply NavigationAttributesControllerAdvice only to user related controllers

--- src/main/java/com/bookstore/web/ui/controller/user/advice/NavigationAttributesControllerAdvice.java
+++ src/main/java/com/bookstore/web/ui/controller/user/advice/NavigationAttributesControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-@ControllerAdvice
+@ControllerAdvice("com.bookstore.web.ui.controller.user")
 public class NavigationAttributesControllerAdvice {
 
     private CategoryService categoryService;
