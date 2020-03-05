968a40704c7cc26e0d9503c9ced4ecb1fd194ebc
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2020-03-05 21:08:14 +0100

    Add admin console main page

---
+++ src/main/java/com/bookstore/web/ui/controller/admin/DashboardController.java
@@ -0,0 +1,14 @@
+package com.bookstore.web.ui.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String get() {
+        return "admin/dashboard";
+    }
+
+}
