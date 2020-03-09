2a70e0c338cb7bcab7974874f0cff1ecd9f26609
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2020-03-09 21:05:37 +0100

    Add equals and hashcode to Book DTO

--- src/main/java/com/bookstore/model/dto/Book.java
+++ src/main/java/com/bookstore/model/dto/Book.java
@@ -3,6 +3,7 @@ package com.bookstore.model.dto;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Data
 public class Book {
@@ -24,5 +25,18 @@ public class Book {
     private String category;
 
     private Long coverId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
