b025b918d44e038658622b5c677ea2c77f5e6fd0
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2020-03-21 18:51:15 +0100

    Enable caching of static resources and book covers

--- src/main/java/com/bookstore/web/ui/controller/BookCoverController.java
+++ src/main/java/com/bookstore/web/ui/controller/BookCoverController.java
@@ -2,12 +2,15 @@ package com.bookstore.web.ui.controller;
 
 import com.bookstore.service.BookCoverService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,7 +23,11 @@ class BookCoverController {
     public ResponseEntity<byte[]> getBookCover(@PathVariable Long coverId) {
         return bookCoverService.findById(coverId)
                 .map(cover ->
-                        ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(cover.getBytes()))
+                        ResponseEntity
+                                .ok()
+                                .contentType(MediaType.IMAGE_JPEG)
+                                .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS))
+                                .body(cover.getBytes()))
                 .orElseGet(() ->
                         ResponseEntity.notFound().build());
     }
